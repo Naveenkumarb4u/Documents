a0999e35122cc0e4ad1c4e8907633d3b5ea78720
Naveen Kumar <naveenkumar6565@gmail.com>
2025-01-09 10:22:34 +0530

    Re-arranged files

---
+++ Useful_Documents/Maven/infonetApp/src/main/java/com/bmw/infonet/App.java
@@ -0,0 +1,13 @@
+package com.bmw.infonet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
